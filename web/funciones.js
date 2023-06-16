639850713e66178177581e393b1c8a2e4124d220
Pablo Iglesias Pose <117298226+Pabloipo@users.noreply.github.com>
2023-06-16 10:29:46 +0200

    Editado funciones.js

--- web/funciones.js
+++ web/funciones.js
@@ -6,6 +6,12 @@ function crearFila(){
 	var table = document.getElementById("tabla");
 	var row = table.insertRow(x);
 	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	var cell5 = row.insertCell(4);
+	var cell6 = row.insertCell(5);
+	var cell7 = row.insertCell(6);
 	
 	for(i=0;i<datos.length;i++){
 		element1.options[i+1]=new Option(datos[i]);
