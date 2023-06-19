e520b89a13adb1f9460ea45e4a92b966c1d3e0ee
unknown <pabloipo01@gmail.com>
2023-06-19 14:15:03 +0200

    [LF-23] Si
    Si

--- web/suscripcion.php
+++ web/suscripcion.php
@@ -1,4 +1,16 @@
 <?php
-    $email = $_POST['email'];
-        echo '<script>alert("Gracias por suscribirte a nuestra newsletter"); window.location = "index.html";</script>'
+// Obtener los datos del formulario
+$email = $_POST['email'];
+
+include("conecta.php");
+		$sql = "INSERT INTO lf_newsletter (correo) VALUES ('$email')";
+
+        if(mysqli_query($conn, $sql)) {
+            echo "Suscripción exitosa. Gracias por suscribirte a nuestra newsletter.";
+        } else {
+            echo "Error al suscribirse. Por favor, inténtalo nuevamente.";
+        }
+
+// Mostrar el mensaje de confirmación y redirigir a la página principal
+echo '<script>alert("Se ha registrado su correo para recibir notificaciones de nuestra newsletter"); window.location = "index.html";</script>';
 ?>
